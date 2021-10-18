980be5c9a8140f334539f0d3d761f876bc9fd872
bjy291 <qor291@gmail.com>
2021-10-18 16:12:29 +0900

    csv파일 정보 DB insert

--- route/controller/chatController.js
+++ route/controller/chatController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const chat = require('../model/chat')
 const multer = require("multer")
+var db=require('../../db')
 router.get('/', chat.main)
 router.post('/chat',chat.chat)
 router.get('/upload', chat.upload);
@@ -27,8 +28,19 @@ router.post('/upload', upload.single("csvfile"), function(req, res, next){
 
     let result={
         originalName :file.originalname,
+        dname: file.fieldname,
         size : file.size
     }
+    
+    sql="insert into csvfile(csv_name, csv_filedname, csv_size) values(?, ?, ?)"
+    db.query(sql, [result.originalName, result.dname, result.size], (err, result) =>{
+        if(err){
+            console.log("csv File insert Err!!!!")
+        }else{
+            console.log(result)
+        }
+    })
+
     res.json(result);
 });
 
